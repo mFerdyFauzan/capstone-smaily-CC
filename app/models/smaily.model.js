367f39238b65838026411778dd2b394d873f6d26
Mochamad Ferdy Fauzan <mochamad.ferdy@ui.ac.id>
2022-05-15 04:15:33 +0700

    update model

--- app/models/smaily.model.js
+++ app/models/smaily.model.js
@@ -1,15 +1,36 @@
 module.exports = (sequelize, Sequelize) => {
     const User = sequelize.define("user", {
+        id: {
+            type: Sequelize.UUID,
+            primaryKey: true,
+            allowNull: false,
+            defaultValue: Sequelize.UUIDV4
+        },
         username: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isAlphanumeric: true
+            }
         },
         password: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            allowNull: false
+            /*
+            validate: {
+                is: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)[a-zA-Z\\d]{8,}$/,
+                isAlphanumeric: true
+            }
+            */
         },
         email: {
             type: Sequelize.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                isEmail: true
+            }
         }
     });
     return User;
